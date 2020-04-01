5e5700450e6d8eab548608f9f5d4f6d0956fd21a
名取凌真 <ryoma-natori@hands-lab.com>
2020-04-01 05:46:10 +0000

    TODOリストまで作った

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app>
+      <!-- マテリアルUIはv-appタグで全体を囲むこと!!囲まないと色がおかしくなったりします。 -->
+      <AppBar></AppBar>
+      <!-- AppBarコンポーネントをサイトより拾ってくる。 -->
+      <InputTask v-on:child-event="TaskAdded" />
+      <taskView v-bind:tasks="tasks" v-on:child-event="TaskFinished" />
+    </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskView from "./components/TaskView";
+import InputTask from "./components/InputTask";
+import AppBar from "./components/AppBar"; // AppBarコンポーネントをサイトより拾ってくる。
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    TaskView,
+    InputTask,
+    AppBar // コンポーネントを追加
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+//import Vuetify from 'vuetify' // 追加したライブラリを読み込む
+import vuetify from './plugins/vuetify';
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
+//Vue.use(Vuetify) // Vueライブラリは使用を宣言。グローバルスコープ。
 
 new Vue({
+  vuetify,
   render: h => h(App),
 }).$mount('#app')
